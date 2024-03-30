58e872f3dcb4f020bfdb1813b9b3e541c199453c
Ashish0417 <ashishkalgutkar413@gmail.com>
2024-03-30 19:16:28 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open('README.md','r',encoding='utf-8') as f:
+    long_description=f.read()
+
+__version__="0.0.0"
+REPO_NAME="Text_summerizer"
+AUTHOR_NAME='Ashish0417'
+SRC_REPO='textsummerizer'
+AUTHOR_EMAIL='ashishkalgutkar413@gmail.com'
--- template.py
+++ template.py
@@ -23,7 +23,7 @@ list_of_files=[
     "apps.py",
     "main.py",
     "Dockerfile",
-    "requirements.py",
+    "requirements.txt",
     "setup.py",
     "research/trials.ipynb"
 
